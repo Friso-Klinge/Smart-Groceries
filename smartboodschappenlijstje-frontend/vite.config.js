21c7fa6559d61995a46217627a6c1ee88e5573ca
Atakan Karaca <atakan.karaca@student.nhlstenden.com>
2026-06-12 10:14:27 +0200

    Merge Conflict Fix

--- smartboodschappenlijstje-frontend/vite.config.js
+++ smartboodschappenlijstje-frontend/vite.config.js
@@ -4,7 +4,15 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/css/auth.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/auth.css',
+                'resources/dashboard.css',
+                'resources/css/my-lists.css',
+                'resources/css/navigation.css',
+                'resources/js/app.js',
+                'resources/js/route-map.js',
+            ],
             refresh: true,
         }),
     ],
